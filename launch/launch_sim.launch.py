9350d51e30993cecbbdbbd864a307a69ac66cfa8
Simon Burfield <sb@compsoft.com>
2024-11-21 13:19:25 +0000

    Fix for new Gazebo

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -44,16 +44,16 @@ def generate_launch_description():
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
+                    get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
+                    launch_arguments={'gz_args': ['-r -v4 ', world], 'on_exit_shutdown': 'true'}.items()
              )
 
-    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+    # Run the spawner node from the ros_gz_sim package. The entity name doesn't really matter if you only have a single robot.
+    spawn_entity = Node(package='ros_gz_sim', executable='create',
                         arguments=['-topic', 'robot_description',
-                                   '-entity', 'ros_bot'],
+                                   '-name', 'my_bot',
+                                   '-z', '0.1'],
                         output='screen')
-
 
     diff_drive_spawner = Node(
         package="controller_manager",
