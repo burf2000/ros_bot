2830cf378f9ef7e27b5df7fb6037ecd9324d435e
Simon Burfield <sb@compsoft.com>
2025-10-13 12:14:12 +0100

    Tweaks to camera and odom settings

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -65,6 +65,7 @@ def generate_launch_description():
         executable="ros2_control_node",
         parameters=[{'robot_description': robot_description},
                     controller_params_file]
+        # remappings=[('/diff_cont/odom', '/odom')]
     )
 
     delayed_controller_manager = TimerAction(period=5.0, actions=[controller_manager])
