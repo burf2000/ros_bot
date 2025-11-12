d247b1ba8689f783a5fc866f3ebfec8c9d1037e0
Simon Burfield <burf2000@gmail.com>
2025-11-12 13:58:02 +0000

    again

--- launch/lidar.launch.py
+++ launch/lidar.launch.py
@@ -1,16 +1,7 @@
-import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-
-    package_name = 'ros_bot'
-    laser_filter_config = os.path.join(
-        get_package_share_directory(package_name),
-        'config',
-        'laser_filter.yaml'
-    )
 
     return LaunchDescription([
         # Lidar publishes scan data directly to /scan
@@ -22,21 +13,9 @@ def generate_launch_description():
                 'port': '/dev/ttyACM0',
                 'frame_id': 'laser_frame'
             }]
-        ),
+        )
 
-        # TODO: Install laser_filters package, then enable this:
+        # TODO: Install laser_filters for noise reduction:
         # sudo apt install ros-humble-laser-filters
-        #
-        # # Laser scan filter cleans up noise
-        # # Reads /scan_raw, outputs /scan (for SLAM and Nav2)
-        # Node(
-        #     package='laser_filters',
-        #     executable='scan_to_scan_filter_chain',
-        #     output='screen',
-        #     parameters=[laser_filter_config],
-        #     remappings=[
-        #         ('/scan', '/scan'),           # Output
-        #         ('/scan_raw', '/scan_raw')    # Input
-        #     ]
-        # )
+        # Then uncomment the laser filter node configuration
     ])
